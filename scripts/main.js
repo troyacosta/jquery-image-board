8de116a28bc71a615c472d310a6a56dfcd045498
Troy Acosta <troyacosta@sbcglobal.net>
2015-09-17 17:39:44 -0500

    push

--- scripts/main.js
+++ scripts/main.js
@@ -4,36 +4,66 @@ $(document).ready(function() {
     var $imageInput = $('#imageInput');
     var $imageCaption = $('#imageCaption');
     var $inputForm = $('#inputForm');
-    var url = 'http://tiyfe.herokuapp.com/collections/troy-pic-site'
+    var $imageUpdate = $('.image');
+    var $captionUpdate = $('.caption');
+    var $imageSection = $('#imageSection');
+    var url = 'http://tiyfe.herokuapp.com/collections/troy-pic-site';
 
     $('#startButton').click(function() {
         $inputForm.slideToggle('slow');
     });
 
-    $('#cancelButton').click(function(e) {
-        e.preventDefault();
+    function clearFields() {
         $imageInput = $imageInput.val('');
         $imageCaption = $imageCaption.val('');
+    }
+    function updateImages() {
+        $imageUpdate = $imageUpdate.val('');
+        $captionUpdate = $captionUpdate.val('');
+    }
+
+    $('#cancelButton').click(function(e) {
+        e.preventDefault();
+        clearFields();
         $inputForm.hide('slow');
     });
 
     $('#addImageButton').click(function(e) {
         e.preventDefault();
+        // clearFields();
+        $inputForm.hide('slow');
         var image = $imageInput.val();
         var caption = $imageCaption.val();
+        console.log(image);
+        console.log(caption);
         $.post(
             url, {
                 image: image,
                 caption: caption
             },
-            function(response) {
-                console.log(response);
+            function(data) {
+                $imageSection.append('<div class="$imageUpdate"><img src="'+data.image+
+                    '"></div><div class="$captionUpdate">'+data.caption+'</div>')
             },
             'json'
-        )
+        );
+        updateImages();
+        
     });
 
-
+    $.get(
+        url,
+        function(data) {
+            data.forEach(function(data) {
+                $imageSection.append('<div class="$imageUpdate"><img src="'+data.image+
+                    '"></div><div class="$captionUpdate">'+data.caption+'</div>')
+            })
+        },
+        function(data){
+            console.log(data);
+        },
+        'json'
+        );
 
 
 
